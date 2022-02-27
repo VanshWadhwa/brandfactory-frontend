8c8391f32ff49bad05d6d494aadc2e83cf1a81a7
Vansh Wadhwa <vanshwadhwasphere@gmail.com>
2022-02-28 00:30:26 +0530

    Footer Added

--- src/components/layout/StickyFooter.js
+++ src/components/layout/StickyFooter.js
@@ -1,16 +1,21 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
+import { ButtonGroup, Divider, Grid, IconButton } from "@mui/material";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="#">
+        Brand Folder
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -28,7 +33,6 @@ export default function StickyFooter() {
       }}
     >
       {/* <CssBaseline /> */}
-
       <Box
         component="footer"
         sx={{
@@ -41,10 +45,63 @@ export default function StickyFooter() {
               : theme.palette.grey[800],
         }}
       >
+        <Grid container>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Typography variant="h4" component="h4" gutterBottom>
+              Brand Folder
+            </Typography>
+            <Typography variant="p" component="p" gutterBottom>
+              Make content creation easier
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6}>
+            <Typography variant="h6" component="h6" gutterBottom>
+              Get Connected
+            </Typography>
+            {/* <Link to="/editor"> */}
+            <ButtonGroup>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <InstagramIcon />
+              </IconButton>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <FacebookIcon />
+              </IconButton>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <TwitterIcon />
+              </IconButton>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <WhatsAppIcon />
+              </IconButton>
+            </ButtonGroup>
+            <a href="#"></a>
+            {/* </Link> */}
+          </Grid>
+        </Grid>
+
+        <br />
+        <Divider variant="middle" />
+        <br />
+
         <Container maxWidth="sm">
-          <Typography variant="body1">
+          {/* <Typography variant="body1">
             My sticky footer can be found here.
-          </Typography>
+          </Typography> */}
           <Copyright />
         </Container>
       </Box>
